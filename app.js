8d4c2c75c172d4b63bdb3dda48e96742c575690f
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-02-15 18:08:33 +0530

    backend of listings of wanderlust

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing")
+const Listing = require("./models/listing.js");
+const path = require("path")
+const methodOverride  = require("method-override")
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
+
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"))
 
 main()
-  .then(() => console.log("connected to db"))
-  .catch((err) => console.log(err));
+.then(() => console.log("connected to db"))
+.catch((err) => console.log(err));
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
@@ -14,21 +22,66 @@ app.get("/", (req, res) => {
   res.send("server started");
 });
 
-app.get("/testlisting",async (req,res)=>{
-  let sampleListing = new Listing({
-    title:"new villa",
-    description : "near beach",
-    price: 23232,
-    location:"goa",
-    country:"india",
+//index route-list of all hotels
+app.get("/listings",async (req,res)=>{
+  let allListings = await Listing.find({});
+  res.render("./listings/index.ejs",{allListings});
 
-  })
-
-  await sampleListing.save()
-  console.log("listed");
-  res.send("gotcha");
 })
 
-app.listen("8080", () => {
+//create new listing - handle request from user
+app.get("/listings/new",(req,res)=>{
+  res.render("./listings/new.ejs");
+})
+
+app.post("/listings", async (req,res)=>{
+  const newlisting = new Listing(req.body.listing)
+  await newlisting.save()
+  res.redirect("/listings")
+})
+
+//detail of a hotel 
+app.get("/listings/:id", async(req,res)=>{
+   let {id} = req.params;
+   let listing = await Listing.findById(id);
+   res.render("./listings/show.ejs",{listing});
+})
+
+app.get("/listings/:id/edit",async (req,res)=>{
+  let {id} = req.params;
+  let listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs",{listing});
+
+})
+
+app.put("/listings/:id",async (req,res)=>{
+let{id} = req.params;
+await Listing.findByIdAndUpdate(id,{...req.body.listing}); // parsing by ...
+res.redirect(`/listings/${id}`)
+})
+
+
+app.delete("/listings/:id", async(req,res)=>{
+  let {id} =req.params;
+  let deletedlisting = await Listing.findByIdAndDelete(id);
+  res.redirect("/listings")
+})
+
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "new villa",
+    description: "near beach",
+    price: 23232,
+    location: "goa",
+    country: "india",
+  });
+  await sampleListing.save();
+  console.log("listed");
+  res.send("gotcha");
+  });
+
+
+
+ app.listen("8080", () => {
   console.log("server listening to port 8080 ");
-});
+ });
